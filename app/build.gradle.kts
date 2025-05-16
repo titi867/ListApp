78a2a83631419335e714eebe1dc18c301d1909ce
titi867 <titi.araneda@gmail.com>
2025-05-16 19:31:09 +0200

    últimos cambios 16.05

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/listapp/FormFragment.kt
+++ app/src/main/java/com/example/listapp/FormFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.activity.addCallback
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 
-class formFragment : Fragment() {
+class FormFragment : Fragment() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +25,15 @@ class formFragment : Fragment() {
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
             findNavController().navigateUp()
         }
+
         return inflater.inflate(R.layout.fragment_form, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val ivPhoto = view.findViewById<ImageView>(R.id.ivPhoto)
+
+        Glide.with(requireContext()).load("https://goo.gl/gEgYUd").into(ivPhoto)
     }
 }
